e41f7881f84c60807e2fdc6801558b1de9332b31
InkonIo <loke90@bk.ru>
2026-05-10 02:15:23 +0500

    new dom

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 // const BASE_URL = 'https://examweb.up.railway.app/api/v1'
 
-const BASE_URL = 'http://localhost:8080/api/v1'
+const BASE_URL = 'https://saintweb.up.railway.app/api/v1'
 
 const authHeaders = () => ({
   'Content-Type': 'application/json',
